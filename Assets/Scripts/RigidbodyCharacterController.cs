ac39668c49beb96280b4ad3e87ed5aa8228709c2
qnf613 <gurwls613@gmail.com>
2020-09-17 21:57:58 -0500

    Basic movement with AddForce

---
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyCharacterController : MonoBehaviour
+{
+    [SerializeField] private float accelerationForce = 10;
+    private new Rigidbody rb;
+    private Vector2 input;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        var inputDirection = new Vector3(input.x, 0, input.y);
+        rb.AddForce(inputDirection * accelerationForce);
+    }
+
+
+    private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+    }
+}
