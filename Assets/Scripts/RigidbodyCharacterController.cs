13d81e020c51a310ae1b996f35c54a8c944a7903
qnf613 <gurwls613@gmail.com>
2020-09-17 22:39:55 -0500

    Physics materials added

--- Assets/Scripts/RigidbodyCharacterController.cs
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -6,24 +6,37 @@ public class RigidbodyCharacterController : MonoBehaviour
 {
     [SerializeField] private float accelerationForce = 10;
     [SerializeField] private float maxSpeed = 2;
+    [SerializeField] private PhysicMaterial stopping, moving;
 
     private new Rigidbody rb;
+    private new Collider collider;
     private Vector2 input;
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        collider = GetComponent<Collider>();
     }
 
     private void FixedUpdate()
     {
         var inputDirection = new Vector3(input.x, 0, input.y);
-        
+
+        collider.material = inputDirection.magnitude > 0 ? moving : stopping;
+        //line 26 is simplified code writing of the line 28 to 35
+        //if (inputDirection.magnitude > 0)
+        //{
+        //    collider.material = moving;
+        //}
+        //else
+        //{
+        //    collider.material = stopping;
+        //}
+
         if (rb.velocity.magnitude < maxSpeed)
         {
-            rb.AddForce(inputDirection * accelerationForce);
+            rb.AddForce(inputDirection * accelerationForce, ForceMode.Acceleration);
         }
     }
 
@@ -32,6 +45,5 @@ public class RigidbodyCharacterController : MonoBehaviour
     {
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
-
     }
 }
