ae0bf6934765331f962bc0b4ad6184df32ac513e
qnf613 <gurwls613@gmail.com>
2020-09-28 15:03:20 -0500

    rotating player to face movement direction
    + small adjustment of the camera

--- Assets/Scripts/RigidbodyCharacterController.cs
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -6,13 +6,12 @@ public class RigidbodyCharacterController : MonoBehaviour
 {
     [SerializeField] private float accelerationForce = 10;
     [SerializeField] private float maxSpeed = 2;
+    [SerializeField] [Tooltip("How fast the player turns. 0 = no turning, 1 = instant turning")] [Range (0,1)] private float turnSpeed = .1f;
     [SerializeField] private PhysicMaterial stopping, moving;
 
     private new Rigidbody rb;
     private new Collider collider;
     private Vector2 input;
-
-
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -21,16 +20,18 @@ public class RigidbodyCharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //detect the input direction
         var inputDirection = new Vector3(input.x, 0, input.y);
-
+        //detect the direction that camera is looking at
         Vector3 camFlattenedFoward = Camera.main.transform.forward;
         camFlattenedFoward.y = 0;
         var camRotarion = Quaternion.LookRotation(camFlattenedFoward);
-
+        //calculate the direction that where player object suppose to go
         Vector3 camRelativeInputDir = camRotarion * inputDirection;
 
+        //change the physics material depending on player's movement status
         collider.material = inputDirection.magnitude > 0 ? moving : stopping;
-        //line 26 is simplified code writing of the line 28 to 35
+        //line 34 is simplified code writing of the line 36 to 43
         //if (inputDirection.magnitude > 0)
         //{
         //    collider.material = moving;
@@ -40,10 +41,20 @@ public class RigidbodyCharacterController : MonoBehaviour
         //    collider.material = stopping;
         //}
 
+        //add force to the player object in direction the camera is looking at
         if (rb.velocity.magnitude < maxSpeed)
         {
             rb.AddForce(camRelativeInputDir * accelerationForce, ForceMode.Acceleration);
         }
+
+        if (inputDirection.magnitude > 0)
+        {
+            //give us back the direction of player's moving to get the player to face the way they're moving
+            var targetRotation = Quaternion.LookRotation(camRelativeInputDir);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed);
+
+        }
+
     }
 
 
