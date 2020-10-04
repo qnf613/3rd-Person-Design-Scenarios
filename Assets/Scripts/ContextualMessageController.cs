1b586dd7c28df48f72d5fa2c6918053e771041c0
qnf613 <gurwls613@gmail.com>
2020-10-04 15:07:15 -0500

    Set up a basic coroutine for ContextualMessage showing / hiding

--- Assets/Scripts/ContextualMessageController.cs
+++ Assets/Scripts/ContextualMessageController.cs
@@ -7,7 +7,7 @@ public class ContextualMessageController : MonoBehaviour
 {
     private CanvasGroup canvasGroup;
     private TMP_Text messageText;
-
+    [SerializeField] private float textDuration;
 
     private void Start()
     {
@@ -15,12 +15,19 @@ public class ContextualMessageController : MonoBehaviour
         messageText = GetComponent<TMP_Text>();
 
         canvasGroup.alpha = 0;
+
+        StartCoroutine(ShowMessage("Testing", textDuration));
+
     }
 
 
-    private void ShowMessage(string message, float duration)
+    private IEnumerator ShowMessage(string message, float duration)
     {
+        canvasGroup.alpha = 1;
         messageText.text = message;
+        //wait for duration
+        yield return new WaitForSeconds(duration);
+        canvasGroup.alpha = 0;
     }
 
 }
