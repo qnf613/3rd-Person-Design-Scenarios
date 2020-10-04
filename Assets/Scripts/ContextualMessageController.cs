a1e75351b144ff8c2d5dcd22df5f9ae8f6781664
qnf613 <gurwls613@gmail.com>
2020-10-04 14:27:57 -0500

    Created contextual message controller script and stubbed in ShowMessage()
    Working Progress!

---
+++ Assets/Scripts/ContextualMessageController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ContextualMessageController : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    private TMP_Text messageText;
+
+
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        messageText = GetComponent<TMP_Text>();
+
+        canvasGroup.alpha = 0;
+    }
+
+
+    private void ShowMessage(string message, float duration)
+    {
+        messageText.text = message;
+    }
+
+}
